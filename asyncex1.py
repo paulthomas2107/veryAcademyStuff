324ea3c85e9f8437bfd05fd17213ec1b2d60ad12
Paul Thomas <paulthomas2107@gmail.com>
2023-09-29 13:15:01 +0100

    More

--- asyncex1.py
+++ asyncex1.py
@@ -4,8 +4,14 @@ import asyncio
 async def task1():
     print('1st email')
     asyncio.create_task(task2())
-    await asyncio.sleep(2)
+    await asyncio.sleep(5)
     print('1st sent')
+
+
+async def send_mail():
+    print('Test')
+    await asyncio.sleep(2)
+    print('Awake')
 
 
 async def task2():
@@ -17,8 +23,25 @@ async def task2():
 
 async def task3():
     print('3rd email')
-    await asyncio.sleep(2)
+    await asyncio.sleep(1)
     print('3rd sent')
+    print("End")
 
 
-asyncio.run(task1())
+async def t1():
+    await t2()
+    print('t1')
+
+
+async def t2():
+    await t3()
+    print('t2')
+
+
+async def t3():
+    print('t3')
+
+
+# print(asyncio.iscoroutinefunction(test1()))
+asyncio.run(send_mail())
+asyncio.run(t1())
